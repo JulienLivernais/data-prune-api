4eca8df538e120758b0d0d3e98d966bf9be84e8b
JulienLivernais <julien.livernais@hotmail.fr>
2026-07-27 19:58:57 +0900

    Add core config and database connection

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+class Settings(BaseSettings):
+    # noinspection PyArgumentList
+    model_config = ConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra='ignore'
+    )
+
+    DATABASE_URL: str
+
+# noinspection PyArgumentList
+settings = Settings()
+
